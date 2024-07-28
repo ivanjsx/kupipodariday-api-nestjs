c50e0cd8265541bdcdb30c77cd3a6f9569129433
Ivan Shanin <ivanxshanin@gmail.com>
2024-07-28 18:05:24 +0300

    import orm module into app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 // decorators
 import { Module } from '@nestjs/common';
 
-// modules
+// app modules
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { WishesModule } from './wishes/wishes.module';
 import { OffersModule } from './offers/offers.module';
 import { WishlistsModule } from './wishlists/wishlists.module';
+
+// orm modules
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 // content
 
@@ -17,6 +20,17 @@ import { WishlistsModule } from './wishlists/wishlists.module';
     WishesModule,
     OffersModule,
     WishlistsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'student',
+      password: 'student',
+      database: 'kupipodariday',
+      schema: 'public',
+      entities: [],
+      synchronize: true,
+    }),
   ],
 })
 export class AppModule {}
