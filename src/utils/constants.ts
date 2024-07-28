286bf26add7d115b407341a81ee95538daeba33d
Ivan Shanin <ivanxshanin@gmail.com>
2024-07-28 20:31:16 +0300

    create baseline wish entity

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,5 @@
+// users
+
 export const MIN_USERNAME_LENGTH = 2;
 export const MAX_USERNAME_LENGTH = 30;
 
@@ -6,3 +8,11 @@ export const MAX_USER_ABOUT_LENGTH = 200;
 
 export const DEFAULT_USER_ABOUT = 'Пока ничего не рассказал о себе';
 export const DEFAULT_USER_AVATAR = 'https://i.pravatar.cc/300';
+
+// wishes
+
+export const MIN_WISH_NAME_LENGTH = 1;
+export const MAX_WISH_NAME_LENGTH = 250;
+
+export const MIN_WISH_DESCRIPTION_LENGTH = 1;
+export const MAX_WISH_DESCRIPTION_LENGTH = 1024;
--- src/wishes/entities/wish.entity.ts
+++ src/wishes/entities/wish.entity.ts
@@ -1,1 +1,87 @@
-export class Wish {}
+// decorators
+import { Column, Entity } from 'typeorm';
+import { IsNumber, IsString, IsUrl, Length } from 'class-validator';
+
+// entities
+import { User } from 'src/users/entities/user.entity';
+import { Offer } from 'src/offers/entities/offer.entity';
+
+// utils
+import { WithIdAndDates } from 'src/utils/entities/with-id-and-dates';
+
+// constants
+import {
+  MIN_WISH_DESCRIPTION_LENGTH,
+  MAX_WISH_DESCRIPTION_LENGTH,
+  MIN_WISH_NAME_LENGTH,
+  MAX_WISH_NAME_LENGTH,
+} from 'src/utils/constants';
+
+// content
+
+@Entity()
+export class Wish extends WithIdAndDates {
+  @IsString()
+  @Length(MIN_WISH_NAME_LENGTH, MAX_WISH_NAME_LENGTH)
+  @Column({
+    length: MAX_WISH_NAME_LENGTH,
+  })
+  name: string;
+
+  @IsUrl({
+    protocols: ['http', 'https'],
+  })
+  @Column()
+  link: string;
+
+  @IsUrl({
+    protocols: ['http', 'https'],
+  })
+  @Column()
+  image: string;
+
+  @IsNumber({
+    allowNaN: false,
+    allowInfinity: false,
+    maxDecimalPlaces: 2,
+  })
+  @Column({
+    scale: 2,
+  })
+  price: number;
+
+  @IsNumber({
+    allowNaN: false,
+    allowInfinity: false,
+    maxDecimalPlaces: 2,
+  })
+  @Column({
+    scale: 2,
+  })
+  raised: number;
+
+  @IsString()
+  @Length(MIN_WISH_DESCRIPTION_LENGTH, MAX_WISH_DESCRIPTION_LENGTH)
+  @Column({
+    length: MAX_WISH_DESCRIPTION_LENGTH,
+  })
+  description: string;
+
+  @IsNumber({
+    allowNaN: false,
+    allowInfinity: false,
+    maxDecimalPlaces: 0,
+  })
+  @Column({
+    scale: 0,
+  })
+  copied: number;
+
+  @Column()
+  // TODO define relation
+  owner: User;
+
+  @Column()
+  // TODO define relation
+  offers: Array<Offer>;
+}
