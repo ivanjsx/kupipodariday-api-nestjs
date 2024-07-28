3c3c94cde2bdddbdfcbda0d9845e74a07e5825eb
Ivan Shanin <ivanxshanin@gmail.com>
2024-07-28 18:01:29 +0300

    generate required nest resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
+// decorators
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+
+// modules
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { WishesModule } from './wishes/wishes.module';
+import { OffersModule } from './offers/offers.module';
+import { WishlistsModule } from './wishlists/wishlists.module';
+
+// content
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [],
+  imports: [
+    AuthModule,
+    UsersModule,
+    WishesModule,
+    OffersModule,
+    WishlistsModule,
+  ],
 })
 export class AppModule {}
