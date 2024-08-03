38dd23c9bb889e4cd5bd610e91f9ec40a80f33ba
Ivan Shanin <ivanxshanin@gmail.com>
2024-08-03 22:34:34 +0200

    configure authentication (access token issue)

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,23 @@
+// decorators
 import { Injectable } from '@nestjs/common';
 
+// providers
+import { JwtService } from '@nestjs/jwt';
+
+// entities
+import { User } from 'src/users/user.entity';
+
+// types
+import { AccessTokenResponse } from 'src/utils/types';
+
+// content
+
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly jwtService: JwtService) {}
+
+  authenticate(user: User): AccessTokenResponse {
+    const payload = { subject: user.username };
+    return { access_token: this.jwtService.sign(payload) };
+  }
+}
