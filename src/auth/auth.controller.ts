e5ada5caf44347608967a3b61794188ebc54a01a
Ivan Shanin <ivanxshanin@gmail.com>
2024-08-03 22:26:29 +0200

    create auth controller with signup route handler

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+// decorators
+import { Body, Controller, Post } from '@nestjs/common';
 
-@Controller('auth')
+// providers
+import { UsersService } from 'src/users/users.service';
+
+// entities
+import { User } from 'src/users/user.entity';
+
+// data transfer objects
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+
+// content
+
+@Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post('signup')
+  signUp(@Body() data: CreateUserDto): Promise<User> {
+    return this.usersService.createOne(data);
+  }
 }
